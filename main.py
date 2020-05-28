6a447d19d58f1b04d0652007a54981b0d3df5269
Kalldrek <65857983+Kalldrek@users.noreply.github.com>
2020-05-28 22:46:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Token = ('863654335:AAE3IMFm9TNkshA1p3x0wudg2BmAWbqOfYs')
 bot = telebot.TeleBot(Token)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
 def start_message(message):
     bot.send_message(message.chat.id, message.text)
 
